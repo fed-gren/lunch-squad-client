763d768a826d82be6bcad9fde319e5977af63396
fed-gren <deveb22@gmail.com>
2019-09-15 17:31:40 +0900

    feat: restaurant info list
    - 현재 필터링 되서 보이는 마커들만 list로 보여줌

--- src/components/MapPageComponents/InfoView/index.jsx
+++ src/components/MapPageComponents/InfoView/index.jsx
@@ -1,32 +1,55 @@
-import React from "react";
+import React, { useContext } from "react";
 import Styled from "./styles";
-import { data } from "../../../../config";
 import { FaRegThumbsUp } from "react-icons/fa";
+import { MapPageContext } from "../../../contexts/MapPageContext";
+import { RestaurantDataContext } from "../../../contexts/RestaurantDataContext";
 //components
 import InfoButtonPartView from "../InfoButtonPartView";
 
 export default function index() {
+  const { restaurantsShowFlags } = useContext(MapPageContext);
+  const {
+    koRestaurants,
+    jpRestaurants,
+    chRestaurants,
+    wsRestaurants
+  } = useContext(RestaurantDataContext);
+
+  const arr = [
+    { foodType: "한식", list: koRestaurants },
+    { foodType: "일식", list: jpRestaurants },
+    { foodType: "중식", list: chRestaurants },
+    { foodType: "양식", list: wsRestaurants }
+  ];
+
+  const filteredList = arr
+    .filter(({ foodType }) => restaurantsShowFlags[foodType])
+    .map(({ list }) => list)
+    .reduce((acc, cur) => acc.concat(cur), []);
+
   return (
     <Styled.Info>
       <ul>
-        {data.dummyList.map(item => (
-          <Styled.InfoItem key={item.id}>
-            <Styled.LikePart>
-              <FaRegThumbsUp />
-              <p className="like">{item.like}</p>
-            </Styled.LikePart>
-            <Styled.DescPart>
-              <Styled.DescTextPart>
-                <h3>{item.name}</h3>
-                <div>
-                  <span>추천메뉴:{item.recommendedMenu}</span>
-                  <span>가격:{item.price}</span>
-                </div>
-              </Styled.DescTextPart>
-              <InfoButtonPartView id={item.id} />
-            </Styled.DescPart>
-          </Styled.InfoItem>
-        ))}
+        {filteredList.map(
+          ({ _id, name, recommend, recommendedMenu, price }) => (
+            <Styled.InfoItem key={_id}>
+              <Styled.LikePart>
+                <FaRegThumbsUp />
+                <p className="like">{recommend}</p>
+              </Styled.LikePart>
+              <Styled.DescPart>
+                <Styled.DescTextPart>
+                  <h3>{name}</h3>
+                  <div>
+                    <span>추천메뉴:{recommendedMenu}</span>
+                    <span>가격:{price}원</span>
+                  </div>
+                </Styled.DescTextPart>
+                <InfoButtonPartView id={_id} />
+              </Styled.DescPart>
+            </Styled.InfoItem>
+          )
+        )}
       </ul>
     </Styled.Info>
   );
