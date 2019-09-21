21e5ccf482afb16d1fe8bbd9a96b83ecdfe74c33
fed-gren <deveb22@gmail.com>
2019-09-21 17:36:18 +0900

    styles: add min-width
    - 화면 크기 줄어들어도 컨텐츠 이상하게 보이지 않도록 스타일 부여

--- src/components/InfoLayout/styles.js
+++ src/components/InfoLayout/styles.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const InfoLayout = styled.section`
   width: 25rem;
+  min-width: 25rem;
   height: 100%;
   overflow: scroll;
   border-left: 1px solid #000;
