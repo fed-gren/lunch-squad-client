0f2acd8e0fa2a84417ed918e386d41b398413aa4
fed-gren <deveb22@gmail.com>
2019-09-23 16:05:20 +0900

    feat: add DetailsView

---
+++ src/components/DetailComponents/DetailsView/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import InfoLayout from "../../InfoLayout";
+// import RestaurantListView from "../RestaurantListView";
+
+export default function styles() {
+  return <InfoLayout></InfoLayout>;
+}
