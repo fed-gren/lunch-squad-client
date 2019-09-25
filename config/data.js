0cef33e911d229182cee9bd91491b90553993a39
fed-gren <deveb22@gmail.com>
2019-09-25 15:26:20 +0900

    chore: remove unusing data

--- config/data.js
+++ config/data.js
@@ -1,111 +1,12 @@
 const data = {
-  ncpClientId: "04lxzjey4k",
-  // lunchSquadApiUrl: "https://goodbye-codesquadbot.herokuapp.com/showAll",
   lunchSquadApiUrl:
     "https://ldlyo4kkqc.execute-api.ap-northeast-2.amazonaws.com/dev/restaurants",
-  restaurantApiUrl: "https://goodbye-codesquadbot.herokuapp.com/show/",
   kakaoMapConfig: {
     defaultCenter: {
       lat: 37.490826,
       lng: 127.033419
     }
-  },
-  dummyList: [
-    {
-      id: 1,
-      name: "aa식당",
-      recommendedMenu: "A찌개",
-      price: "8000원",
-      like: 29
-    },
-    {
-      id: 2,
-      name: "bb식당",
-      recommendedMenu: "B말이",
-      price: "3000원",
-      like: 9
-    },
-    {
-      id: 3,
-      name: "cc식당",
-      recommendedMenu: "C튀김",
-      price: "10000원",
-      like: 3
-    },
-    {
-      id: 4,
-      name: "dd식당",
-      recommendedMenu: "D버거",
-      price: "6500원",
-      like: 2
-    },
-    {
-      id: 5,
-      name: "ee식당",
-      recommendedMenu: "E국",
-      price: "7000원",
-      like: 1
-    },
-    {
-      id: 6,
-      name: "ff식당",
-      recommendedMenu: "F밥",
-      price: "7200원",
-      like: 0
-    }
-  ],
-  dummyReviews: [
-    {
-      id: 1,
-      username: "아무개",
-      review:
-        "싸고 맛있어요! 근데 아쉬운 점이 식당 분위기가 조금 어두워요. 그런데 뭐 그런거 신경 쓸 겨를 있나요? ^^ 그냥 맛있게 먹으면 됩니다. ㅎㅎ",
-      tasteScore: 4,
-      priceScore: 4,
-      regDate: "2019-07-20"
-    },
-    {
-      id: 2,
-      username: "홍길동",
-      review: "김치찌개가 진짜 너무 맛있네..",
-      tasteScore: 5,
-      priceScore: 3,
-      regDate: "2018-11-12"
-    },
-    {
-      id: 3,
-      username: "라이언",
-      review: "흠..내입맛엔 별로. 가격은 괜춘!",
-      tasteScore: 2,
-      priceScore: 4,
-      regDate: "2018-02-05"
-    },
-    {
-      id: 4,
-      username: "황무지",
-      review: "메뉴가 왤케 없는지 모르겠음..",
-      tasteScore: 1,
-      priceScore: 2,
-      regDate: "2019-03-21"
-    },
-    {
-      id: 5,
-      username: "앙몬드",
-      review:
-        "그냥 다 좋아~ 근데 한 가지 아쉬운 점은 주인 아저씨가 티비 리모콘을 잡고 절대 놔주지 않음. 그래서 혹시라도 보고싶은 프로그램은 절대로 못봄. 게다가 너무 재미없는 프로를 보시니까 혼자 온 사람은 유튜브 재밌는거 챙겨가세요 ^^",
-      tasteScore: 5,
-      priceScore: 5,
-      regDate: "2019-05-01"
-    },
-    {
-      id: 6,
-      username: "따릉이",
-      review: "자전거 타고 가기에 좋음~",
-      tasteScore: 5,
-      priceScore: 3,
-      regDate: "2017-07-11"
-    }
-  ]
+  }
 };
 
 export default data;
