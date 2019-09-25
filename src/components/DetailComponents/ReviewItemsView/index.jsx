f729dc98d33638929f7d4df2a09e7454a7ca07dd
fed-gren <deveb22@gmail.com>
2019-09-25 23:58:43 +0900

    feat: apply review data
    - 서버에서 받아온 리뷰 데이터로 교체

--- src/components/DetailComponents/ReviewItemsView/index.jsx
+++ src/components/DetailComponents/ReviewItemsView/index.jsx
@@ -1,18 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import Styled from "./styles";
+import { RestaurantContext } from "../../../contexts/RestaurantContext";
 
 export default function index() {
-  const dummyReviews = [
-    "평냉계의 신화라고들 함 ",
-    "맛있어요. 소고기도 맛있는데 엄청 비쌈 ",
-    "전 맛있게 먹음",
-    "아... 노맛",
-    "서비스가 좋았습니다.",
-    "또 가고 싶네요"
-  ];
+  const { selectedRestaurant } = useContext(RestaurantContext);
+  const { review: reviews } = selectedRestaurant;
+
   return (
-    dummyReviews &&
-    dummyReviews.map((c, idx) => {
+    reviews &&
+    reviews.map((c, idx) => {
       return (
         <Styled.ReviewItem key={idx}>
           <p>{c}</p>
