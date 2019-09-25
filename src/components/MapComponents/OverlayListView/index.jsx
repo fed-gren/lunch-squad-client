af96865b5ad8157a414da1432a2191d251dd8198
fed-gren <deveb22@gmail.com>
2019-09-26 01:30:01 +0900

    feat: set center
    - 리스트에서 식당 선택 시 map center 설정

--- src/components/MapComponents/OverlayListView/index.jsx
+++ src/components/MapComponents/OverlayListView/index.jsx
@@ -1,11 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { CustomOverlay } from "react-kakao-maps";
 import { RestaurantContext } from "../../../contexts/RestaurantContext";
-
 import RestaurantOverlay from "../RestaurantOverlay";
+import KakaoMapContext from "react-kakao-maps";
 
 export default () => {
-  const { filteredRestaurants } = useContext(RestaurantContext);
+  const { filteredRestaurants, selectedRestaurant } = useContext(
+    RestaurantContext
+  );
+  const { kakaoMapObj, map } = useContext(KakaoMapContext);
+
+  useEffect(() => {
+    if (!selectedRestaurant || !map) return;
+    const { lat, lng } = selectedRestaurant;
+
+    map.setCenter(new kakaoMapObj.maps.LatLng(lat, lng));
+  }, [selectedRestaurant]);
 
   return (
     filteredRestaurants &&
