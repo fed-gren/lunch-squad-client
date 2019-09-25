780d22cdf44cd0afeb942ac5677c80f8d68a8410
fed-gren <deveb22@gmail.com>
2019-09-25 11:19:21 +0900

    feat: apply custom overlay
    - 마커 대신 커스텀 오버레이 사용

--- src/components/MapComponents/MarkerListView/index.jsx
+++ src/components/MapComponents/MarkerListView/index.jsx
@@ -1,13 +1,21 @@
 import React, { useContext } from "react";
-import { Marker } from "react-kakao-maps";
+import { CustomOverlay } from "react-kakao-maps";
 import { RestaurantContext } from "../../../contexts/RestaurantContext";
+
+import RestaurantOverlay from "../RestaurantOverlay";
+
 export default () => {
   const { restaurants } = useContext(RestaurantContext);
 
   return (
     restaurants &&
-    Object.values(restaurants).map(({ id, lat, lng }) => (
-      <Marker key={id} {...{ lat, lng }}></Marker>
+    Object.values(restaurants).map(({ id, lat, lng, restaurantName }) => (
+      <CustomOverlay
+        key={id}
+        content={<RestaurantOverlay message={restaurantName} />}
+        {...{ lat, lng }}
+        yAnchor={1}
+      ></CustomOverlay>
     ))
   );
 };
---
+++ src/components/MapComponents/RestaurantOverlay/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const RestaurantOverlay = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #1abc9c;
+  color: #fff;
+  padding: 0.3rem;
+  border-radius: 0.4rem;
+
+  &:after {
+    content: "";
+    position: absolute;
+    border-top: 10px solid #1abc9c;
+    border-right: 5px solid transparent;
+    border-left: 5px solid transparent;
+    bottom: -9px;
+    left: calc(50% - 5px);
+  }
+`;
+
+export default { RestaurantOverlay };
