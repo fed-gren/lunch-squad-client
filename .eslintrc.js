3a7cf3b10943984787bdb41e23a2d369db6b3020
fed-gren <deveb22@gmail.com>
2019-10-18 00:55:59 +0900

    env: install eslint
    - eslint 설치 및 airbnb 설정 적용

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "airbnb"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
