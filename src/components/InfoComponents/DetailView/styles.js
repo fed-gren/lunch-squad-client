86aa9a27f0167ffe14eea2de029bbf48fdd3d7db
fed-gren <deveb22@gmail.com>
2019-10-18 00:37:30 +0900

    style: modify height of restaurant list, detail view
    - 식당 목록, 상세정보 height 일치

--- src/components/InfoComponents/DetailView/styles.js
+++ src/components/InfoComponents/DetailView/styles.js
@@ -2,13 +2,13 @@ import styled from "styled-components";
 
 const DetailView = styled.div`
   background-color: #f9f9f9;
-  height: 32rem;
+  height: 36rem;
   width: 100%;
   overflow: scroll;
   position: absolute;
   top: 100%;
   /* TODO: 리스트 접기, 펼치기 기능 추가 시 top 조절*/
-  /* top: calc(-32rem + 100%); */
+  /* top: calc(-36rem + 100%); */
   left: 0;
 `;
 
